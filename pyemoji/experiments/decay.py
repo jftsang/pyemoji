34ccdc465330b76371b078f39f5451822af68821
GitHub Action <action@github.com>
2026-04-11 12:08:59 +0100

    Stuff for looking at ensembles. Fix MFT thingy

--- pyemoji/experiments/decay.py
+++ pyemoji/experiments/decay.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from scipy.integrate import solve_ivp
 from tqdm.auto import tqdm
 
 from pyemoji.actions import GoToStateAction, IfNeighborAction, IfRandomAction
@@ -24,6 +25,8 @@ upstate = State(id=1, icon="🔴", name="up", actions=[])
 # Additional probability of decay if you have a neighbour who has
 # already decayed.
 
+tmax = 300
+
 base_rate = 0.01
 additional_rate = 0.01
 neighbors_needed_for_assistance = 4
@@ -44,10 +47,10 @@ rules = Model(
     world=WorldRules(
         neighborhood="neumann",
         proportions=[{"stateID": 0, "parts": 0}, {"stateID": 1, "parts": 100}],
-        height=103,
-        width=101,
-        # height=29,
-        # width=31,
+        # height=59,
+        # width=61,
+        height=29,
+        width=31,
     ),
 )
 
@@ -56,7 +59,6 @@ class DecaySim(Simulator):
     def __init__(self, *a, **k):
         super().__init__(*a, **k)
         self.pop_history = []
-        self.tmax = 1000
 
     def post_step(self):
         t = self.time
@@ -64,46 +66,65 @@ class DecaySim(Simulator):
         self.pop_history.append({"t": t, **p})
 
     def should_stop(self) -> bool:
-        return self.populations()["up"] <= 50 or self.time > self.tmax
+        # return self.populations()["up"] <= 10 or self.time > tmax
+        return self.time >= tmax
 
     def finalize(self):
+        pass
+
+
+def main():
+    ax = plt.gca()
+    simulator: DecaySim
+    n_experiments = 20
+    t = np.arange(0, tmax)
+    results = np.empty((n_experiments, len(t)))
+    for sid in range(n_experiments):
+        simulator = DecaySim(rules)
+
+        for _ in tqdm(simulator.run(), total=tmax):
+            pass
         df = pd.DataFrame.from_records(simulator.pop_history)
-        t = df["t"].to_numpy()
+        # t = df["t"].to_numpy()
         pop = df["up"].to_numpy()
+        results[sid, :] = pop
 
-        # Number predicted by exponential decay
-        exp_decay = self.grid.size * np.exp(-base_rate * t)
+        cmap = plt.get_cmap("Oranges")
+        ax.plot(t, pop, "-", c=cmap(sid / n_experiments), lw=0.8)
 
-        ax = plt.gca()
-        ax.plot(t, pop, "r-", label="actual population")
-        ax.plot(
-            t,
-            exp_decay,
-            "k-",
-            label="exponential decay (base rate)",
-        )
-        ax.plot(
-            t,
-            self.grid.size * np.exp(-(base_rate + additional_rate) * t),
-            "k:",
-            label="exponential decay (always assisted)",
-        )
+    mu = np.mean(results, axis=0)
+    std = np.std(results, axis=0)
+    ax.fill_between(t, mu - std, mu + std, alpha=0.2, color="r")
+    ax.plot(t, mu, "r-")
+    ax.plot(t, mu + std, "r--")
+    ax.plot(t, mu - std, "r--")
 
-        # Mean field theory predicts...
-        delta = additional_rate / base_rate
-        r0 = base_rate
-        # mft_p = np.exp(-base_rate * t) / (1 - delta * np.exp(-base_rate * t))
-        mft_p = (np.exp(4 * r0 * t) + delta * (np.exp(4 * r0 * t) - 1)) ** -0.25
-        ax.plot(t, self.grid.size * mft_p, "k-.", label="mean field theory")
+    gs = rules.world.height * rules.world.width
+    exp_decay = gs * np.exp(-base_rate * t)
+    ax.plot(t, exp_decay, "k-", label="exponential decay (base rate)")
+    ax.plot(
+        t,
+        gs * np.exp(-(base_rate + additional_rate) * t),
+        "k:",
+        label="exponential decay (always assisted)",
+    )
 
-        ax.set_ylim(50 * 0.95, self.grid.size * 1.05)
-        ax.set_yscale("log")
-        ax.legend()
-        plt.show()
+    # Mean field theory predicts...
+    delta = additional_rate / base_rate
+    r0 = base_rate
 
+    def mft(_, p):
+        return -r0 * p * (1 + delta * (1 - p) ** 4)
 
-simulator = DecaySim(rules)
+    sol = solve_ivp(mft, [0, tmax], [1], t_eval=np.arange(0, tmax))
+
+    ax.plot(sol.t, gs * sol.y[0], "k-.", label="mean field theory")
+
+    ax.set_ylim(50, gs * 1.05)
+    ax.set_yscale("log")
+    ax.legend()
+    plt.show()
+
 
 if __name__ == "__main__":
-    for _ in tqdm(simulator.run(), total=simulator.tmax):
-        pass
+    main()
